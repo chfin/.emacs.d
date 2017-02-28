8746676d548e0ffbe8a513a61d0dac9c05237b73
Christoph Finkensiep <chfin@freenet.de>
2017-02-28 22:27:51 +0100

    create backups in ~/.emacs.d/saves

--- init.el
+++ init.el
@@ -30,7 +30,18 @@
 ;;;; general settings ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;use tabs
 (setq-default indent-tabs-mode nil)
+
+;; put backups in a meaningful place
+(setq
+   backup-by-copying-when-linked t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("" . "~/.emacs.d/saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
 
 ;;(load-theme 'monokai t)
 
